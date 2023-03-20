c3d795b700bec2a8cbaa20b805e2909fee5e9fb7
Andrea Franz <andrea@gravityblast.com>
2023-03-20 16:51:09 +0100

    add abstract base client

--- src/index.ts
+++ src/index.ts
@@ -36,22 +36,58 @@ const voteBuilder = (obj: any): Vote => ({
   amount: obj.amount,
 });
 
-
-class AlloIndexerClient {
+abstract class BaseClient {
   public fetch: typeof fetch;
   public baseURI: string;
   public chainId: number;
 
-  public readonly routes: { [name: string]: string } = {
+  protected abstract routes: { [name: string]: string };
+
+  constructor(fetchImpl: typeof fetch, baseURI: string, chainId: number) {
+    this.fetch = fetchImpl;
+    this.baseURI = baseURI;
+    this.chainId = chainId;
+  }
+
+  protected fetchResources<T>(builder: ResourceBuilder<T>, url: string): Promise<T[]> {
+    return fetch(url)
+      .then(resp => resp.json())
+      .then(list => list.map((obj: Array<any>) => builder(obj)));
+  }
+
+  protected fetchResourceFromList<T>(builder: ResourceBuilder<T>, url: string, key: keyof T, value: any): Promise<T | undefined> {
+    return this.fetchResources(builder, url)
+      .then((list: T[]) => list.find((r: T) => r[key] === value));
+  }
+
+  protected buildURL(routeName: string, params: { [key: string]: RouteParamValue }): string {
+    const path = this.compileRoute(this.routes[routeName], {
+      ...params,
+      chainId: this.chainId,
+    });
+
+    return new URL(path, this.baseURI).toString();
+  }
+
+  protected compileRoute(route: string, params: { [key: string]: RouteParamValue }): string {
+    let slug = route;
+    for (const key in params) {
+      slug = slug.replace(`:${key}`, params[key].toString());
+    }
+
+    return slug;
+  }
+}
+
+class AlloIndexerClient extends BaseClient {
+  protected routes: { [name: string]: string } = {
     projects: "/data/:chainId/projects.json",
     rounds: "/data/:chainId/rounds.json",
     roundVotes: "/data/:chainId/rounds/:roundId/votes.json",
   };
 
   constructor(fetchImpl: typeof fetch, baseURI: string, chainId: number) {
-    this.fetch = fetchImpl;
-    this.baseURI = baseURI;
-    this.chainId = chainId;
+    super(fetchImpl, baseURI, chainId);
   }
 
   getProjects(): Promise<Project[]> {
@@ -76,41 +112,12 @@ class AlloIndexerClient {
 
     return this.fetchResources(voteBuilder, url);
   }
-
-  private fetchResources<T>(builder: ResourceBuilder<T>, url: string): Promise<T[]> {
-    return fetch(url)
-      .then(resp => resp.json())
-      .then(list => list.map((obj: Array<any>) => builder(obj)));
-  }
-
-  private fetchResourceFromList<T>(builder: ResourceBuilder<T>, url: string, key: keyof T, value: any): Promise<T | undefined> {
-    return this.fetchResources(builder, url)
-      .then((list: T[]) => list.find((r: T) => r[key] === value));
-  }
-
-  private buildURL(routeName: string, params: { [key: string]: RouteParamValue }): string {
-    const path = this.compileRoute(this.routes[routeName], {
-      ...params,
-      chainId: this.chainId,
-    });
-
-    return new URL(path, this.baseURI).toString();
-  }
-
-  private compileRoute(route: string, params: { [key: string]: RouteParamValue }): string {
-    let slug = route;
-    for (const key in params) {
-      slug = slug.replace(`:${key}`, params[key].toString());
-    }
-
-    return slug;
-  }
 }
 
 (async () => {
   const c = new AlloIndexerClient(fetch, "http://localhost:4000", 1);
-  // const res = await c.getProjects();
-  const res = await c.getProject("projectNumber", 1);
+  const res = await c.getProjects();
+  // const res = await c.getProject("projectNumber", 1);
   // const res = await c.getRounds();
   // const res = await c.getRoundVotes("0xD95A1969c41112cEE9A2c931E849bCef36a16F4C");
   console.log(res);

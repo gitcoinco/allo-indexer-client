6673a2881fd14af05b1c6c2e74172149a26a5db2
hans <hmrtn@pm.me>
2023-04-19 17:07:13 -0700

    fix: resolved requested changes

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -32,9 +32,9 @@ const roundsCommand = (_args: { [key: string]: string }) =>
     .then((rounds) => rounds.forEach((r) => console.log(r)))
     .catch(logError);
 
-const projectCommand = (_args: { [key: string]: string }) =>
+const projectCommand = (args: { [key: string]: string }) =>
   client
-    .getProjectBy("projectNumber", Number(_args.projectNumber))
+    .getProjectBy("projectNumber", Number(args.projectNumber))
     .then((project) => console.log(project))
     .catch(logError);
 
@@ -107,6 +107,6 @@ await cmd.handler(values);
 //   // const res = await c.getRoundApplications("0xD95A1969c41112cEE9A2c931E849bCef36a16F4C");
 //   const res = await c.getRoundApplicationBy("0xD95A1969c41112cEE9A2c931E849bCef36a16F4C", "id", "0xc290dd8e51ac35480d9872ce4484aac23bb812c47c0567bfd4beb9113726ed11");
 //   // const res = await c.getVotes("0xe575282b376E3c9886779A841A2510F1Dd8C2CE4");
-//   // const res = await c.getVotes("0xe575282b376E3c9886779A841A2510F1Dd8C2CE4"), "0xA0000000000000000000000000000000000000000000000000000000000001"); 
+//   // const res = await c.getVotes("0xe575282b376E3c9886779A841A2510F1Dd8C2CE4"), "0xA0000000000000000000000000000000000000000000000000000000000001");
 //   console.log(res);
 // })()

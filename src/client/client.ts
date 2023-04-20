0493c1bc01615186b2d2c212a390242ab86c116e
Mohamed Boudra <boudra.moha@gmail.com>
2023-04-20 12:59:24 +0200

    feat: add applications and matches

--- src/client/client.ts
+++ src/client/client.ts
@@ -10,10 +10,13 @@ import { Project, Round, Vote, Application } from "./types.js";
 export class Client extends BaseClient {
   protected routes: { [name: string]: string } = {
     projects: "/data/:chainId/projects.json",
-    projectVotes: "/data/:chainId/rounds/:roundId/projects/:projectId/votes.json",
+    projectVotes:
+      "/data/:chainId/rounds/:roundId/projects/:projectId/votes.json",
     rounds: "/data/:chainId/rounds.json",
     roundVotes: "/data/:chainId/rounds/:roundId/votes.json",
-    roundApplications: "/data/:chainId/rounds/:roundId/projects.json",
+    roundApplications: "/data/:chainId/rounds/:roundId/applications.json",
+    roundProjects: "/data/:chainId/rounds/:roundId/projects.json",
+    roundMatchingFunds: "/data/:chainId/rounds/:roundId/matches",
   };
 
   constructor(fetchImpl: typeof fetch, baseURI: string, chainId: number) {
@@ -77,6 +80,22 @@ export class Client extends BaseClient {
       key,
       value,
       caseSensitive
+    );
+  }
+
+  getRoundMatchingFunds(roundId: string): Promise<Application[]> {
+    return this.fetchResources(
+      "roundMatchingFunds",
+      { roundId },
+      roundApplicationBuilder
+    );
+  }
+
+  getRoundProjects(roundId: string): Promise<Application[]> {
+    return this.fetchResources(
+      "roundProjects",
+      { roundId },
+      roundApplicationBuilder
     );
   }
 
